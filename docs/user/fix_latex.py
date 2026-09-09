da66ff8dc8a481e38e0b4f1bd29ff264e2bcbf7b
Jacek Kosek <jacek.kosek@iter.org>
2026-09-09 16:53:53 +0200

    Table continue test

--- docs/user/fix_latex.py
+++ docs/user/fix_latex.py
@@ -21,13 +21,13 @@ content = content.replace(
 count_ttt = content.count(r'\begin{tabulary}{\linewidth}[t]{TTT}')
 content = content.replace(
     r'\begin{tabulary}{\linewidth}[t]{TTT}',
-    r'\begin{tabulary}{\linewidth}[t]{p{3.2cm}p{2cm}p{11cm}}'
+    r'\begin{tabulary}{\linewidth}[t]{p{3.2cm}p{2cm}p{9cm}}'
 )
 
 count_tttt = content.count(r'\begin{tabulary}{\linewidth}[t]{TTTT}')
 content = content.replace(
     r'\begin{tabulary}{\linewidth}[t]{TTTT}',
-    r'\begin{tabulary}{\linewidth}[t]{p{3.2cm}p{2cm}p{3cm}p{8cm}}'
+    r'\setlength{\tymax}{5cm} \begin{tabulary}{\linewidth}[t]{p{3.2cm}p{2cm}p{3cm}L}'
 )
 
 count_ttttt = content.count(r'\begin{tabulary}{\linewidth}[t]{TTTTT}')
